0ce8ffaba5fab17c9d678157da859498091e2128
Teddy Economou <teddyecon@gmail.com>
2023-02-22 11:21:07 -0500

    completed

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib_list = [0,1]
+    i = 2
+    while i < length:
+        fib_list.append( fib_list[i-1] + fib_list[i-2] )
+        i += 1
+    
+
+    print(fib_list[0:length])
